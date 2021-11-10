e5a0d85b0d52bda91df927c83f3d77723581bf10
isatera <kinesintera@gmail.com>
2021-11-11 04:10:42 +0900

    Modified templates to display posts from database.

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,19 @@
 from django.shortcuts import render
+from django.utils import timezone
+
+# models の前にあるドットは カレントディレクトリ 、もしくは カレントアプリケーション のことです。
+from .models import Post
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    """
+    表示したいデータを取り出して、テンプレートファイルに渡す
+    request: インターネットを介してユーザから受け取った全ての情報が詰まったもの
+    """
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+
+    # {}: 指定した情報を、テンプレートが表示してくれます。
+    # {} の中に引数を記述する時は、名前と値をセットにしなくてはなりません。
+    return render(request, 'blog/post_list.html', {'posts':posts})
+    
+
+
